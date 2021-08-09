9c0bace5054ab059cc9c11ae1ead54d5c8d92997
Yashwanthnaini <46812344+Yashwanthnaini@users.noreply.github.com>
2021-08-09 12:20:34 +0530

    added our mission page

--- src/mypages/AboutUs/OurMission/Mission.js
+++ src/mypages/AboutUs/OurMission/Mission.js
@@ -1,12 +1,45 @@
 import React from 'react';
+import {Container,CentralContainer,CentralContent1,CentralContent2 ,BoldContent,Innercontainer,Images,ImagesMobile,ContentOnImage,Imagecaption,Moreitems,Items,Symbol,ContentBox,Content,Content1,SideContent} from "./Mission.styles";
 
-function Mission() {
+
+
+const  Mission = () => {
     return (
-        <div style={{alignItems: "center",
-        padding: "50px"
-        }}>
-            <center><h3>Mission Page</h3></center>
-         </div>
+        <div >
+            <Container >
+                <CentralContainer >
+                    <CentralContent1>
+                        Our Mission
+                    </CentralContent1>
+                    <CentralContent2>
+                        Shaping the inclusive growth around the world.
+                    </CentralContent2>
+                </CentralContainer>
+                <ContentBox >
+                    <BoldContent>
+                        Inclusive Growth and Why does our work matter?
+                    </BoldContent>
+                    <Content >
+                   
+                        <Content1 >
+                            For a long time, the traditional pattern of development has significantly promoted economic growth and globalization. On the other hand, it has also faced many challenges. The most important issues that we are facing today are income inequality and the wealth gap, as well as severe environmental pollution and waste of resources. In all cases, there is likely to be a misallocation of resources caused by unbalanced, extensive economic growth.
+                        </Content1>
+                        <Content1 >
+                            The latest development model known as “inclusive growth” is a practical solution to these problems. The goal of such growth is to strike a balance between economic and sustainable development. In other words, instead of only focusing on the economic outcomes as in traditional models, inclusive growth focuses more on equity. The interests of all parties are closely interconnected after the tidal wave of globalization sweeping the world. Only inclusive growth can address the root of global challenges and maintain the international order. The prerequisite for such growth is to open markets around the world, as promoting global competition and innovation is crucial to our prosperity. Also, it will boost development and global governance by turning the world into a community of interests and responsibilities. In short, inclusive growth will be focused more on the relationship between man and man, or man and nature, instead of capital growth and accumulation.      
+                        </Content1>   
+                        <Content1 >
+                            Nowadays, without the new breakthroughs that come from technological innovation, the economy is recovering slowly. In the meantime, it still faces many uncertainties such as the rise of protectionism, populism and geopolitical conflict. Inclusive growth perfectly facilitates the stability and development of the global economy. On the one hand, inclusive growth will add new impetus and vitality, providing new room for economic growth. The rapid development of technologies that facilitate inclusivity has led to the proliferation of e-commerce, mobile Internet, and intelligent manufacturing. Moreover, energy-saving measures and the reduction of carbon emissions are expected to become the steam engine of the 4th Industrial Revolution.
+                        </Content1>
+                        <Content1 >
+                            Still, inclusive growth will continuously reduce the remaining barriers to the free flow of capital, goods, services, and technology among countries and regional blocks, which in the end will provide an environment conducive to economic recovery.
+                        </Content1>
+                        <Content1 >
+                            Inclusive Growth will bring to the world a better and healthy balance, along with a new source of economic strength.
+                        </Content1>
+                    </Content>
+                </ContentBox>
+            </Container> 
+        </div>
     )
 };
 
---
+++ src/mypages/AboutUs/OurMission/Mission.styles.js
@@ -0,0 +1,85 @@
+import styled from 'styled-components'
+export const Container = styled.body`
+margin: 0px;
+height:auto;
+width:100%;
+@media only screen and (max-width: 600px) {
+   width:100%;
+}
+`;
+export const CentralContainer = styled.div`
+margin-top:4rem;
+align-items:center;
+text-align:center;
+@media only screen and (max-width: 600px) {
+    margin-top:2.5rem;
+ }
+`;
+export const CentralContent1 = styled.span`
+
+text-align:center;
+font-size:40px;
+font-family: 'Poppins', sans-serif;
+@media only screen and (max-width: 600px) {
+    font-size:25px;
+ }
+`;
+export const CentralContent2 = styled.p`
+
+text-align:center;
+font-size:15px;
+margin:0px;
+font-family: 'Poppins', sans-serif;
+font-weight: normal;
+@media only screen and (max-width: 600px) {
+    font-size:14px;  
+ }
+`;
+
+export const ContentBox = styled.div`
+margin:50px 35px;
+width:auto;
+height:auto;
+display:flex;
+flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: space-between;
+    @media only screen and (max-width: 600px) {      
+    flex-direction:none;
+    margin: 30px 20px;
+    
+}
+`;
+export const BoldContent = styled.p`
+margin:0px;
+padding:0px;
+width:100%;
+padding-bottom:1rem;
+font-size:40px;
+font-family: 'Poppins', sans-serif;
+@media only screen and (max-width: 600px) {
+    font-size:23px;
+ }
+`;
+export const Content = styled.div`
+width:100%;
+height:auto;
+@media only screen and (max-width: 600px) {      
+    width:100%;
+}
+`;
+export const Content1 = styled.p`
+font-family:helvetica-w01-roman,helvetica-w02-roman,helvetica-lt-w10-roman,sans-serif;
+font-size:17px;
+font-weight: normal;
+
+margin:0px;
+padding:0px;
+padding-bottom:1.6rem;
+color: #141414;
+line-height: 1.8rem;
+@media only screen and (max-width: 600px) {      
+    font-size:17px;
+    line-height: 1.5rem;
+}
+`;
