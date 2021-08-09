42358796910d23c9a2cb5008dba956bb416541cd
Yashwanthnaini <46812344+Yashwanthnaini@users.noreply.github.com>
2021-08-09 15:30:52 +0530

    added our howwedo page

--- src/mypages/AboutUs/OurMission/Mission.styles.js
+++ src/mypages/AboutUs/OurMission/Mission.styles.js
@@ -3,15 +3,16 @@ export const Container = styled.div`
 margin: 0px;
 height:auto;
 width:100%;
-@media only screen and (max-width: 600px) {
-   width:100%;
+
+@media only screen and (max-width: 800px) {
+   
 }
 `;
 export const CentralContainer = styled.div`
 margin-top:4rem;
 align-items:center;
 text-align:center;
-@media only screen and (max-width: 600px) {
+@media only screen and (max-width: 800px) {
     margin-top:2.5rem;
  }
 `;
@@ -20,7 +21,7 @@ export const CentralContent1 = styled.span`
 text-align:center;
 font-size:40px;
 font-family: 'Poppins', sans-serif;
-@media only screen and (max-width: 600px) {
+@media only screen and (max-width: 800px) {
     font-size:25px;
  }
 `;
@@ -31,7 +32,7 @@ font-size:15px;
 margin:0px;
 font-family: 'Poppins', sans-serif;
 font-weight: normal;
-@media only screen and (max-width: 600px) {
+@media only screen and (max-width: 800px) {
     font-size:14px;  
  }
 `;
@@ -44,7 +45,7 @@ display:flex;
 flex-wrap: wrap;
     flex-direction: row;
     justify-content: space-between;
-    @media only screen and (max-width: 600px) {      
+    @media only screen and (max-width: 800px) {      
     flex-direction:none;
     margin: 30px 20px;
     
@@ -57,14 +58,14 @@ width:100%;
 padding-bottom:1rem;
 font-size:40px;
 font-family: 'Poppins', sans-serif;
-@media only screen and (max-width: 600px) {
+@media only screen and (max-width: 800px) {
     font-size:23px;
  }
 `;
 export const Content = styled.div`
 width:100%;
 height:auto;
-@media only screen and (max-width: 600px) {      
+@media only screen and (max-width: 800px) {      
     width:100%;
 }
 `;
@@ -78,7 +79,7 @@ padding:0px;
 padding-bottom:1.6rem;
 color: #141414;
 line-height: 1.8rem;
-@media only screen and (max-width: 600px) {      
+@media only screen and (max-width: 800px) {      
     font-size:17px;
     line-height: 1.5rem;
 }
