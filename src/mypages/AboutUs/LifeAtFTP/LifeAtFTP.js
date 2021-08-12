3f84ab8ee760b7981bab8d58be569d91eba1e936
Yashwanthnaini <46812344+Yashwanthnaini@users.noreply.github.com>
2021-08-12 19:28:29 +0530

    added life at ftp page

--- src/mypages/AboutUs/LifeAtFTP/LifeAtFTP.js
+++ src/mypages/AboutUs/LifeAtFTP/LifeAtFTP.js
@@ -1,12 +1,171 @@
 import React from 'react';
-
+import {Container,HeadContent,Images,Imagecontent,Imagedescription,Imagetitle,VerticalLine1,VerticalLine2,BoldContent,ContentBox,Content,Content1,DoContent,DoContent1,DoContent2,DivSVG1,DivSVG2,DivSVG3,DivSVG4 } from "./LifeAtFTP.styles";
 function LifeAtFTP() {
     return (
-        <div style={{alignItems: "center",
-        padding: "50px"
-        }}>
-            <center><h3> Life At FTP Page</h3></center>
-         </div>
+        <div>
+            <Container>
+                <ContentBox>
+                    <HeadContent>
+                        Join us in Shaping the inclusive growth around the world.
+                    </HeadContent>
+                    <VerticalLine1 />
+                    <Content>
+                        <BoldContent>
+                            Culture of Work
+                        </BoldContent>
+                        <Content1>
+                            FTP strives to cultivate an inclusive and accessible workplace where all people feel comfortable being themselves in a safe and supportive environment. We know that when our members feel empowered to positively contribute to our success, we see more creative problem-solving and innovative solutions.
+                        </Content1>
+                        <Content1 >
+                            We believe diversity of background, ability, experience, and thought makes for a better workplace, better decision-making, and more innovation.
+                               
+                        </Content1>
+                        <VerticalLine2 />
+                                      
+                        <DoContent>
+                            
+                            <DoContent1>
+                                <Images src={"https://static.wixstatic.com/media/7331fc_d2e223a0e91344fa8c571ddb2e4b955f~mv2.jpg/v1/crop/x_605,y_62,w_1728,h_1731/fill/w_300,h_300,al_c,q_80,usm_0.66_1.00_0.01/yvraj.webp"}></Images>
+                                <Imagecontent>
+                                    <Imagetitle>
+                                        – Yuraj Kanni, Chairperson
+                                    </Imagetitle>
+                                </Imagecontent>
+                            </DoContent1>
+                            <DoContent2>
+                                <Imagedescription>
+                                    <DivSVG1>
+                                        <svg preserveAspectRatio="xMidYMid meet" data-bbox="0 19.7 200 161.209" xmlns="http://www.w3.org/2000/svg" viewBox="0 19.7 200 161.209" role="presentation" aria-hidden="true">
+                                            <g>
+                                                <path d="M78.2 19.8L85.7 33c.8 1.4 1.7 2.9 2.7 4.5-3.9 2.3-7.7 4.7-11.5 6.9-12.7 7.3-24.2 16.2-33.4 27.8-8.9 11.2-13 24.1-14.3 38.5 4.4 0 8.6-.4 12.8.1 6.8.7 13.9 1.1 20.3 3.3 13.1 4.4 19.5 14.3 20.7 27.9.6 7.4-.5 14.4-4.2 20.9-7.8 13.6-20.5 19.7-37.3 17.5-21.8-2.6-33.5-13.5-38.7-36.3-1.2-5.3-1.9-10.7-2.8-16v-15.4c.2-.6.5-1.2.6-1.9 1-10 4-19.3 8.8-28.1 11.1-20.4 27-36.3 46.3-48.9 7.2-4.7 14.5-9.4 21.8-14.1.2.1.4.1.7.1z"></path>
+                                                <path d="M189.4 19.8c3.5 5.8 6.9 11.6 10.6 17.8-1.7 1-3.4 2.1-5.1 3-13.4 7.8-26.3 16.1-36.8 27.9-10.7 12-15.9 26-17.4 42.3 1.4 0 2.8-.1 4.2 0 8.3.6 16.8.2 24.8 2.1 18.9 4.4 26.1 18.9 24.8 36.6-1.3 16.9-15.3 30.5-31.9 31.3-12.2.6-23.9-1.1-33.5-9.5-8.7-7.6-12.6-17.9-15.1-28.8-7.2-32.2 2.1-59.7 23.9-83.6 11-12 23.8-21.8 37.6-30.3 4.5-2.8 8.8-5.8 13.3-8.8h.6z"></path>
+                                            </g>
+                                        </svg>
+                                    </DivSVG1>
+                                    At FTP, we’re dedicated to being a world-class and accessible workplace where all our people feel comfortable to work.
+                                    <DivSVG2>
+                                        <svg preserveAspectRatio="xMidYMid meet" data-bbox="0 19.7 200 161.209" xmlns="http://www.w3.org/2000/svg" viewBox="0 19.7 200 161.209" role="presentation" aria-hidden="true">
+                                            <g>
+                                                <path d="M78.2 19.8L85.7 33c.8 1.4 1.7 2.9 2.7 4.5-3.9 2.3-7.7 4.7-11.5 6.9-12.7 7.3-24.2 16.2-33.4 27.8-8.9 11.2-13 24.1-14.3 38.5 4.4 0 8.6-.4 12.8.1 6.8.7 13.9 1.1 20.3 3.3 13.1 4.4 19.5 14.3 20.7 27.9.6 7.4-.5 14.4-4.2 20.9-7.8 13.6-20.5 19.7-37.3 17.5-21.8-2.6-33.5-13.5-38.7-36.3-1.2-5.3-1.9-10.7-2.8-16v-15.4c.2-.6.5-1.2.6-1.9 1-10 4-19.3 8.8-28.1 11.1-20.4 27-36.3 46.3-48.9 7.2-4.7 14.5-9.4 21.8-14.1.2.1.4.1.7.1z"></path>
+                                                <path d="M189.4 19.8c3.5 5.8 6.9 11.6 10.6 17.8-1.7 1-3.4 2.1-5.1 3-13.4 7.8-26.3 16.1-36.8 27.9-10.7 12-15.9 26-17.4 42.3 1.4 0 2.8-.1 4.2 0 8.3.6 16.8.2 24.8 2.1 18.9 4.4 26.1 18.9 24.8 36.6-1.3 16.9-15.3 30.5-31.9 31.3-12.2.6-23.9-1.1-33.5-9.5-8.7-7.6-12.6-17.9-15.1-28.8-7.2-32.2 2.1-59.7 23.9-83.6 11-12 23.8-21.8 37.6-30.3 4.5-2.8 8.8-5.8 13.3-8.8h.6z"></path>
+                                            </g>
+                                        </svg>
+                                    </DivSVG2>
+                                </Imagedescription>
+                                
+                            </DoContent2>
+                            
+                        </DoContent>
+                          
+                    </Content>
+                    <VerticalLine1 /> 
+
+                    <Content>
+                        <BoldContent>
+                        Culture of Learning
+                        </BoldContent>
+                        <Content1>
+                        Our people can pursue dynamic and diverse career journeys, gaining rapid experience and sharpening leadership skills. Flourish in a stimulating environment, with training and development opportunities that are personalized to people objectives.
+                        </Content1>
+                        <Content1 >
+                        Wherever you are in the career, we offer the opportunity to develop in ways that put you ahead of the curve.
+                               
+                        </Content1>
+                        <VerticalLine2 />                       
+                        <DoContent>
+                            
+                            <DoContent1>
+                                <Images src={"https://static.wixstatic.com/media/7331fc_52ff8cbfd07d407c8e0232bdbfee2b92~mv2.jpg/v1/fill/w_300,h_300,al_c,q_80,usm_0.66_1.00_0.01/IMG-20200429-WA0008.webp"}></Images>
+                                <Imagecontent>
+                                    <Imagetitle>
+                                    - Sravan, CEDO
+                                    </Imagetitle>
+                                </Imagecontent>
+                            </DoContent1>
+                            <DoContent2>
+                                <Imagedescription>
+                                    <DivSVG1>
+                                        <svg preserveAspectRatio="xMidYMid meet" data-bbox="0 19.7 200 161.209" xmlns="http://www.w3.org/2000/svg" viewBox="0 19.7 200 161.209" role="presentation" aria-hidden="true">
+                                            <g>
+                                                <path d="M78.2 19.8L85.7 33c.8 1.4 1.7 2.9 2.7 4.5-3.9 2.3-7.7 4.7-11.5 6.9-12.7 7.3-24.2 16.2-33.4 27.8-8.9 11.2-13 24.1-14.3 38.5 4.4 0 8.6-.4 12.8.1 6.8.7 13.9 1.1 20.3 3.3 13.1 4.4 19.5 14.3 20.7 27.9.6 7.4-.5 14.4-4.2 20.9-7.8 13.6-20.5 19.7-37.3 17.5-21.8-2.6-33.5-13.5-38.7-36.3-1.2-5.3-1.9-10.7-2.8-16v-15.4c.2-.6.5-1.2.6-1.9 1-10 4-19.3 8.8-28.1 11.1-20.4 27-36.3 46.3-48.9 7.2-4.7 14.5-9.4 21.8-14.1.2.1.4.1.7.1z"></path>
+                                                <path d="M189.4 19.8c3.5 5.8 6.9 11.6 10.6 17.8-1.7 1-3.4 2.1-5.1 3-13.4 7.8-26.3 16.1-36.8 27.9-10.7 12-15.9 26-17.4 42.3 1.4 0 2.8-.1 4.2 0 8.3.6 16.8.2 24.8 2.1 18.9 4.4 26.1 18.9 24.8 36.6-1.3 16.9-15.3 30.5-31.9 31.3-12.2.6-23.9-1.1-33.5-9.5-8.7-7.6-12.6-17.9-15.1-28.8-7.2-32.2 2.1-59.7 23.9-83.6 11-12 23.8-21.8 37.6-30.3 4.5-2.8 8.8-5.8 13.3-8.8h.6z"></path>
+                                            </g>
+                                        </svg>
+                                    </DivSVG1>
+                                    We provide cognitive-driven learning ecosystem that provides a personalized signature learning experience for our people.
+                                    <DivSVG3>
+                                        <svg preserveAspectRatio="xMidYMid meet" data-bbox="0 19.7 200 161.209" xmlns="http://www.w3.org/2000/svg" viewBox="0 19.7 200 161.209" role="presentation" aria-hidden="true">
+                                            <g>
+                                                <path d="M78.2 19.8L85.7 33c.8 1.4 1.7 2.9 2.7 4.5-3.9 2.3-7.7 4.7-11.5 6.9-12.7 7.3-24.2 16.2-33.4 27.8-8.9 11.2-13 24.1-14.3 38.5 4.4 0 8.6-.4 12.8.1 6.8.7 13.9 1.1 20.3 3.3 13.1 4.4 19.5 14.3 20.7 27.9.6 7.4-.5 14.4-4.2 20.9-7.8 13.6-20.5 19.7-37.3 17.5-21.8-2.6-33.5-13.5-38.7-36.3-1.2-5.3-1.9-10.7-2.8-16v-15.4c.2-.6.5-1.2.6-1.9 1-10 4-19.3 8.8-28.1 11.1-20.4 27-36.3 46.3-48.9 7.2-4.7 14.5-9.4 21.8-14.1.2.1.4.1.7.1z"></path>
+                                                <path d="M189.4 19.8c3.5 5.8 6.9 11.6 10.6 17.8-1.7 1-3.4 2.1-5.1 3-13.4 7.8-26.3 16.1-36.8 27.9-10.7 12-15.9 26-17.4 42.3 1.4 0 2.8-.1 4.2 0 8.3.6 16.8.2 24.8 2.1 18.9 4.4 26.1 18.9 24.8 36.6-1.3 16.9-15.3 30.5-31.9 31.3-12.2.6-23.9-1.1-33.5-9.5-8.7-7.6-12.6-17.9-15.1-28.8-7.2-32.2 2.1-59.7 23.9-83.6 11-12 23.8-21.8 37.6-30.3 4.5-2.8 8.8-5.8 13.3-8.8h.6z"></path>
+                                            </g>
+                                        </svg>
+                                    </DivSVG3>
+                                </Imagedescription>
+                                
+                            </DoContent2>
+                            
+                        </DoContent>
+                          
+                    </Content>
+                    <VerticalLine2 />
+                    
+                    <Content>
+                        <BoldContent>
+                        Culture of Collaboration
+                        </BoldContent>
+                        <Content1>
+                        We focus on cultivating a sense of belonging and an environment where everyone can thrive. Welcoming the new thoughts irrespective to their position and solving problems together which makes our people more interactive and collaborative.
+                        </Content1>
+                        <Content1 >
+                        Growing through close exposure to top leaders from business, government, academia, and civil society makes our people more empowered.
+                               
+                        </Content1>
+                        <VerticalLine2 />                       
+                        <DoContent>
+                            
+                            <DoContent1>
+                                <Images src={"https://static.wixstatic.com/media/7331fc_47def4450efe4346aec9ca9d743678f7~mv2.jpg/v1/crop/x_0,y_27,w_1087,h_1089/fill/w_300,h_300,al_c,q_80,usm_0.66_1.00_0.01/IMG-20200628-WA0014.webp"}></Images>
+                                <Imagecontent>
+                                    <Imagetitle>
+                                    – Uday, CDO
+                                    </Imagetitle>
+                                </Imagecontent>
+                            </DoContent1>
+                            <DoContent2>
+                                <Imagedescription>
+                                    <DivSVG1>
+                                        <svg preserveAspectRatio="xMidYMid meet" data-bbox="0 19.7 200 161.209" xmlns="http://www.w3.org/2000/svg" viewBox="0 19.7 200 161.209" role="presentation" aria-hidden="true">
+                                            <g>
+                                                <path d="M78.2 19.8L85.7 33c.8 1.4 1.7 2.9 2.7 4.5-3.9 2.3-7.7 4.7-11.5 6.9-12.7 7.3-24.2 16.2-33.4 27.8-8.9 11.2-13 24.1-14.3 38.5 4.4 0 8.6-.4 12.8.1 6.8.7 13.9 1.1 20.3 3.3 13.1 4.4 19.5 14.3 20.7 27.9.6 7.4-.5 14.4-4.2 20.9-7.8 13.6-20.5 19.7-37.3 17.5-21.8-2.6-33.5-13.5-38.7-36.3-1.2-5.3-1.9-10.7-2.8-16v-15.4c.2-.6.5-1.2.6-1.9 1-10 4-19.3 8.8-28.1 11.1-20.4 27-36.3 46.3-48.9 7.2-4.7 14.5-9.4 21.8-14.1.2.1.4.1.7.1z"></path>
+                                                <path d="M189.4 19.8c3.5 5.8 6.9 11.6 10.6 17.8-1.7 1-3.4 2.1-5.1 3-13.4 7.8-26.3 16.1-36.8 27.9-10.7 12-15.9 26-17.4 42.3 1.4 0 2.8-.1 4.2 0 8.3.6 16.8.2 24.8 2.1 18.9 4.4 26.1 18.9 24.8 36.6-1.3 16.9-15.3 30.5-31.9 31.3-12.2.6-23.9-1.1-33.5-9.5-8.7-7.6-12.6-17.9-15.1-28.8-7.2-32.2 2.1-59.7 23.9-83.6 11-12 23.8-21.8 37.6-30.3 4.5-2.8 8.8-5.8 13.3-8.8h.6z"></path>
+                                            </g>
+                                        </svg>
+                                    </DivSVG1>
+                                    We always strive to provide right tools and build right environments that encourage collaboration among our members.
+                                    <DivSVG4>
+                                        <svg preserveAspectRatio="xMidYMid meet" data-bbox="0 19.7 200 161.209" xmlns="http://www.w3.org/2000/svg" viewBox="0 19.7 200 161.209" role="presentation" aria-hidden="true">
+                                            <g>
+                                                <path d="M78.2 19.8L85.7 33c.8 1.4 1.7 2.9 2.7 4.5-3.9 2.3-7.7 4.7-11.5 6.9-12.7 7.3-24.2 16.2-33.4 27.8-8.9 11.2-13 24.1-14.3 38.5 4.4 0 8.6-.4 12.8.1 6.8.7 13.9 1.1 20.3 3.3 13.1 4.4 19.5 14.3 20.7 27.9.6 7.4-.5 14.4-4.2 20.9-7.8 13.6-20.5 19.7-37.3 17.5-21.8-2.6-33.5-13.5-38.7-36.3-1.2-5.3-1.9-10.7-2.8-16v-15.4c.2-.6.5-1.2.6-1.9 1-10 4-19.3 8.8-28.1 11.1-20.4 27-36.3 46.3-48.9 7.2-4.7 14.5-9.4 21.8-14.1.2.1.4.1.7.1z"></path>
+                                                <path d="M189.4 19.8c3.5 5.8 6.9 11.6 10.6 17.8-1.7 1-3.4 2.1-5.1 3-13.4 7.8-26.3 16.1-36.8 27.9-10.7 12-15.9 26-17.4 42.3 1.4 0 2.8-.1 4.2 0 8.3.6 16.8.2 24.8 2.1 18.9 4.4 26.1 18.9 24.8 36.6-1.3 16.9-15.3 30.5-31.9 31.3-12.2.6-23.9-1.1-33.5-9.5-8.7-7.6-12.6-17.9-15.1-28.8-7.2-32.2 2.1-59.7 23.9-83.6 11-12 23.8-21.8 37.6-30.3 4.5-2.8 8.8-5.8 13.3-8.8h.6z"></path>
+                                            </g>
+                                        </svg>
+                                    </DivSVG4>
+                                </Imagedescription>
+                                
+                            </DoContent2>
+                            
+                        </DoContent>
+                          
+                    </Content>
+                    <VerticalLine1 />
+
+
+
+
+                </ContentBox>
+            </Container>
+        </div>
     )
 };
 
---
+++ src/mypages/AboutUs/LifeAtFTP/LifeAtFTP.styles.js
@@ -0,0 +1,301 @@
+import styled from 'styled-components'
+export const Container = styled.div`
+margin: 0px;
+height:auto;
+width:100%;
+
+@media only screen and (max-width: 800px) {
+   
+}
+`;
+export const ContentBox = styled.div`
+margin:100px 95px;
+width:auto;
+height:auto;
+display:flex;
+flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: space-between;
+    @media only screen and (max-width: 800px) {      
+    flex-direction:none;
+    margin: 30px 20px;
+}
+`;
+export const HeadContent = styled.span`
+    font-size:36px;
+    width:60%;
+    line-height: 1.4em;
+    font-family: helvetica-w01-bold,helvetica-w02-bold,helvetica-lt-w10-bold,sans-serif;
+    height:auto;
+    padding-bottom:1rem;
+    color:#383232f7;
+    @media only screen and (max-width: 800px) {
+        font-size:17px;
+        width:100%;
+        
+       
+}
+`;
+export const VerticalLine1 = styled.span`
+margin: 0;
+width:100%;
+height:1px;
+background-color:#000;
+@media only screen and (max-width: 800px) {      
+    
+}
+`;
+export const Content = styled.div`
+width:100%;
+height:auto;
+@media only screen and (max-width: 800px) {      
+    width:100%;
+}
+`;
+export const BoldContent = styled.p`
+margin:0px;
+padding:16px 0px 10px 0px;
+width:100%;
+padding-bottom:1rem;
+font-size:16px;
+font-family: 'Poppins', sans-serif;
+@media only screen and (max-width: 800px) {
+    
+ }
+`;
+export const Content1 = styled.p`
+font-family:helvetica-w01-roman,helvetica-w02-roman,helvetica-lt-w10-roman,sans-serif;
+font-size:17px;
+font-weight: normal;
+
+margin:0px;
+padding:0px;
+padding-bottom:1.6rem;
+color: #141414;
+line-height: 1.8rem;
+@media only screen and (max-width: 800px) {      
+    font-size:17px;
+    line-height: 1.5rem;
+}
+`;
+export const VerticalLine2 = styled.div`
+margin: 0;
+width:100%;
+height:1px;
+background-color:#000;
+@media only screen and (max-width: 800px) {      
+    
+}
+`;
+export const DoContent = styled.div`
+margin:35px 35px 0 35px;
+width:auto;
+height:400px;
+display:flex;
+
+@media only screen and (max-width: 800px) {      
+    padding-top:1rem;
+    margin:0;
+    display:inline-block;
+    align-items:center;
+    text-align:center;
+    width:100%;
+    height:auto;
+}
+`;
+export const DoContent1 = styled.span`
+margin:38px 0 0 3.8rem;
+padding:0px;
+width:auto;
+
+font-size:34px;
+color:#005C9C;
+font-family: 'Poppins', sans-serif;
+@media only screen and (max-width: 800px) {
+    width:100%;
+    font-size:25px;
+    margin: 0;
+ }
+`;
+export const Images = styled.img`
+width: 200px;
+height: 200px;
+object-fit: cover;
+margin-left: auto;
+margin-right: auto;
+border-radius:50%;
+over-flow:hidden;
+@media only screen and (max-width: 600px) {
+    height:190px;
+   
+
+}
+
+`;
+export const Imagecontent = styled.div`
+display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    height:220px;
+     padding: 0px;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    -ms-flex-direction: column;
+    flex-direction: column;
+    -webkit-box-flex: 1;
+    -ms-flex-positive: 1;
+    flex-grow: 1;
+    padding-bottom:0px;
+    @media only screen and (max-width: 600px) {
+        height:auto;
+}
+`;
+export const Imagetitle = styled.h5`
+    color: #FF6161;
+    
+    font-family: 'Julius Sans One', sans-serif;
+    font-weight:lighter;
+    font-size:22px;
+    line-height: 1.5em;
+    height: 37px;
+    margin-top:10px;
+    margin-bottom: 0px;
+    letter-spacing: 0.1px;
+    padding-right:10px;
+    
+    @media only screen and (max-width: 600px) {
+        font-size: 20px;
+
+
+} 
+`;
+export const DoContent2 = styled.span`
+width:auto;
+height:auto;
+font-family:helvetica-w01-roman,helvetica-w02-roman,helvetica-lt-w10-roman,sans-serif;
+font-size:17px;
+font-weight: normal;
+
+margin:0px;
+padding:0px 0px 0px 36px;
+
+color: #141414;
+line-height: 1.8rem;
+@media only screen and (max-width: 800px) {      
+    padding:0;
+    width:100%;
+    font-size:20px;
+    margin-bottom:2rem;
+    align-items:center;
+    text-align:center;
+}
+`;
+
+export const DivSVG1 = styled.div`
+transform: rotate(
+    3.626199336382683deg
+    );
+width: 45px;
+height: 36px;
+stroke-width: 0;
+fill-opacity: 1;
+stroke: #ED1566;
+stroke-opacity: 1;
+fill: #FF4040;
+position: relative;
+    right: 46px;
+    top: 23px;
+    @media only screen and (max-width: 600px) {
+        width: 25px;
+        height: 20px;
+        position: relative;
+        right: 28px;
+        top: 13px; 
+}
+`;
+export const Imagedescription = styled.p`
+    font-size:33px;
+    line-height: 1.7em;
+    font-family: helvetica-w01-bold,helvetica-w02-bold,helvetica-lt-w10-bold,sans-serif;
+    font-weight: bold;
+    font-weight: 650;
+    height:105px;
+    width:69%;
+    color:#000;
+    margin:0 0 0 100px;
+    @media only screen and (max-width: 600px) {
+        font-size:17px;
+        align-items:center;
+        text-align: left;
+        margin-left:10%;
+        width:90%;
+
+
+}
+`;
+export const DivSVG2 = styled.div`
+transform: rotate(
+    180deg
+    );
+width: 45px;
+height: 31px;
+stroke-width: 0;
+fill-opacity: 1;
+stroke: #ED1566;
+stroke-opacity: 1;
+fill: #FF4040;
+position: relative;
+    left: 130px;
+    top: -21px;
+    @media only screen and (max-width: 600px) {
+        width: 25px;
+        height: 20px;
+        position: relative;
+                left: 169px;
+                top: -18px; 
+}
+`;
+export const DivSVG3 = styled.div`
+transform: rotate(
+    180deg
+    );
+width: 45px;
+height: 31px;
+stroke-width: 0;
+fill-opacity: 1;
+stroke: #ED1566;
+stroke-opacity: 1;
+fill: #FF4040;
+position: relative;
+    left: 160px;
+    top: -21px;
+    @media only screen and (max-width: 600px) {
+        width: 25px;
+        height: 20px;
+        position: relative;
+                left: 213px;
+                top: -17px;  
+}
+`;
+export const DivSVG4 = styled.div`
+transform: rotate(
+    180deg
+    );
+width: 45px;
+height: 31px;
+stroke-width: 0;
+fill-opacity: 1;
+stroke: #ED1566;
+stroke-opacity: 1;
+fill: #FF4040;
+position: relative;
+    left: 160px;
+    top: -23px;
+    @media only screen and (max-width: 600px) {
+        width: 25px;
+        height: 20px;
+        position: relative;
+                left: 175px;
+                top: -17px;  
+}
+`;
